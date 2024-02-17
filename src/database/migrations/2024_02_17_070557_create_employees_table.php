2d59867ab8ba6c7f22cfe94e8f6fe0d65d60342c
uicklv <uicklv@gmail.com>
2024-02-17 09:14:01 +0200

    add(migrations): add employees and locations migration

---
+++ src/database/migrations/2024_02_17_070557_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('location_id')->constrained();
+            $table->string('name', 150);
+            $table->integer('age')->unsigned();
+            $table->string('email', 250)->unique();
+            $table->string('salary', 250)->nullable();
+            $table->string('position', 350);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
