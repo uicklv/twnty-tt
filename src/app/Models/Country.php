619c73a1958b72f06fc57f5b451ee5d2c24fc2af
uicklv <uicklv@gmail.com>
2024-02-17 13:09:23 +0200

    add(controller): add employee controller, collection, resource

--- src/app/Models/Country.php
+++ src/app/Models/Country.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Country extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
--- src/app/Models/Employee.php
+++ src/app/Models/Employee.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Employee extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
