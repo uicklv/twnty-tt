79833253833bc1fe8e1df8071a4caebdee1b3cdd
uicklv <uicklv@gmail.com>
2024-02-17 13:32:58 +0200

    add(sanctum): add api auth

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * api login
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        // return validation error
+        if($validate->fails()){
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Validation Error',
+                'data' => $validate->errors(),
+            ], 403);
+        }
+
+        $userEmail = $request->get('email');
+        $userPassword = $request->get('password');
+
+        // check email exist
+        $user = User::where('email', $userEmail)->first();
+
+        // check password
+        if(!$user || !Hash::check($userPassword, $user->password)) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        // prepare response
+        $data['token'] = $user->createToken($userEmail)->plainTextToken;
+        $data['user'] = $user;
+
+        $response = [
+            'status' => 'success',
+            'message' => 'User is logged in successfully.',
+            'data' => $data,
+        ];
+
+        return response()->json($response);
+    }
+
+    /**
+     * api logout
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        // delete use tokens
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User is logged out successfully'
+        ]);
+    }
+}
